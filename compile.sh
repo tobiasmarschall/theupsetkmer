ed20e5cee016783890b0801241e66bf6267e5b3e
Tobias Marschall <tobias.marschall@0ohm.net>
2020-03-13 13:46:36 +0100

    Link to Bifrost in conda env

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-g++ -fexceptions -O3 -std=c++11 -march=native -Wno-c++98-compat -c venn_diagram.cpp -o venn_diagram.o
-g++ -o venn_diagram venn_diagram.o -s -O3 -lbifrost -pthread -lz
+#!/bin/bash
+g++ -fexceptions -O3 -std=c++11 -march=native -Wno-c++98-compat -I $CONDA_PREFIX/include -c venn_diagram.cpp -o venn_diagram.o
+g++ -o venn_diagram venn_diagram.o -s -O3 -lbifrost -pthread -lz -L $CONDA_PREFIX/lib -Wl,-rpath,$CONDA_PREFIX/lib
+
